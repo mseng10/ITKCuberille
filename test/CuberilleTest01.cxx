67d25be7cd94cd7f47546331dce96f8b89ca45d7
Mathew Seng <mathewseng@gmail.com>
2020-02-18 07:18:11 -0600

    BUG: Fix failing kwstyle test. Header mismatch.

--- test/CuberilleTest01.cxx
+++ test/CuberilleTest01.cxx
@@ -1,6 +1,6 @@
 /*=========================================================================
  *
- *  Copyright Insight Software Consortium
+ *  Copyright NumFOCUS
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
  *  limitations under the License.
  *
  *=========================================================================*/
+
 #define USE_BSPLINE_INTERPOLATOR 0
 #define USE_MARCHING_CUBES 0
 #define USE_QUAD_EDGE_MESH 1
